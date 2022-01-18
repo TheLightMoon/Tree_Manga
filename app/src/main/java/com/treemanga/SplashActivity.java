1b254a4dde431777daca7de5e98e47eb3a235562
Nguyễn Việt Hà <nguyenviethoangbm9z@gmail.com>
2022-01-18 14:55:16 +0200

    repo: add SplashActivity.java, add activity_splash.xml, add color.xml

--- app/src/main/java/com/treemanga/SplashActivity.java
+++ app/src/main/java/com/treemanga/SplashActivity.java
@@ -2,13 +2,24 @@ package com.treemanga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashActivity extends AppCompatActivity {
+
+    int TIME_PER_SECOND = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        },TIME_PER_SECOND);
     }
 }
